d370d988fcb5acaf4c7369d8d9477b18f6b7b70c
franklinle <frle@ucsd.edu>
2021-09-09 21:59:45 -0700

    News API connected

--- newsApp/views.py
+++ newsApp/views.py
@@ -1,3 +1,54 @@
 from django.shortcuts import render
-
-# Create your views here.
+from newsapi import NewsApiClient
+ 
+ 
+ 
+def Index(request):
+    newsapi = NewsApiClient(api_key="52bf227da152410f9d1ad5ef014b27f3")
+    topheadlines = newsapi.get_top_headlines(sources='al-jazeera-english')
+ 
+ 
+    articles = topheadlines['articles']
+ 
+    desc = []
+    news = []
+    img = []
+ 
+    for i in range(len(articles)):
+        myarticles = articles[i]
+ 
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+ 
+ 
+    mylist = zip(news, desc, img)
+ 
+ 
+    return render(request, 'index.html', context={"mylist":mylist})
+ 
+ 
+ 
+def bbc(request):
+    newsapi = NewsApiClient(api_key="52bf227da152410f9d1ad5ef014b27f3")
+    topheadlines = newsapi.get_top_headlines(sources='bbc-news')
+ 
+ 
+    articles = topheadlines['articles']
+ 
+    desc = []
+    news = []
+    img = []
+ 
+    for i in range(len(articles)):
+        myarticles = articles[i]
+ 
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+ 
+ 
+    mylist = zip(news, desc, img)
+ 
+ 
+    return render(request, 'bbc.html', context={"mylist":mylist})
